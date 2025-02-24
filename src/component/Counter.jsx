57e9e3f18b70db818ac92f647c1e8462b1987e7d
surajshaw08 <krsurajshaw17082004@gmail.com>
2025-02-24 09:00:43 +0530

    Whole div is clickable

--- src/component/Counter.jsx
+++ src/component/Counter.jsx
@@ -4,42 +4,45 @@ import RestartAltTwoToneIcon from '@mui/icons-material/RestartAltTwoTone';
 
 
 const Counter = () => {
-    const [count, setCount] = useState(() =>{
-        const savedCount = localStorage.getItem('counterValue');
-        return savedCount !== null ? Number(savedCount) : 0;
-    });
+  const [count, setCount] = useState(() => {
+    const savedCount = localStorage.getItem('counterValue');
+    return savedCount !== null ? Number(savedCount) : 0;
+  });
 
-    useEffect(()=>{
-        localStorage.setItem('counterValue', count);
-    },[count]);
-    return (
-        <div className="flex flex-col items-center h-screen bg-yellow-400 p-4">
-            <div className=" flex items-center justify-center bg-yellow-400 shadow-lg rounded-[25px] border-8 border-white  w-auto px-4">
-          <h1 className="text-4xl font-bold text-white">{count}</h1>
-        </div>
-        {/* Counter Display
+  useEffect(() => {
+    localStorage.setItem('counterValue', count);
+  }, [count]);
+  return (
+    <div className="flex flex-col items-center h-screen bg-yellow-400 p-4">
+      <div className=" flex items-center justify-center bg-yellow-400 shadow-lg rounded-[25px] border-8 border-white  w-auto px-4">
+        <h1 className="text-4xl font-bold text-white">{count}</h1>
+      </div>
+      {/* Counter Display
         <div className="mb-6 p-4 bg-white shadow-lg rounded-lg">
           <h1 className="text-4xl font-bold">Counter</h1>
         </div> */}
-  
-        {/* Button Section */}
-        <div className="flex flex-col items-center justify-center mt-15 mb-8  bg-white w-[90px] h-[50px] rounded-full">
+
+      {/* Button Section */}
+      <div className="flex flex-col items-center justify-center mt-15 mb-8  bg-white w-[90px] h-[50px] rounded-full">
         <button className='' onClick={() => setCount(0)}
-          >
-            <RestartAltTwoToneIcon/>
-          </button>
-          </div>
-          <div className='flex flex-col items-center w-full mb-8'>
-          <button
-          className=" bg-yellow-400 hover:bg-yellow-400 text-white font-semibold px-6 py-3 rounded-full text-4xl absolute bottom-8 w-[149] border-8 border-white flex items-center justify-center"
-          onClick={() => setCount(count + 1)}
         >
-         Count
+          <RestartAltTwoToneIcon />
         </button>
-          </div>
-          
-        
       </div>
+      {/* Increment Section */}
+      <div
+        className="flex flex-col items-center w-full mb-8 relative h-screen"
+        onClick={() => setCount(count + 1)} // Div click logic
+      >
+        <button
+          className="bg-yellow-400 hover:bg-yellow-400 text-white font-semibold px-6 py-3 rounded-full text-4xl   w-[149px] border-8 border-white flex items-center justify-center absolute bottom-8"
+        >
+          Count
+        </button>
+      </div>
+
+
+    </div>
   );
 };
 
